5e5758bd409965bf4f603823b42d0724dfcb3fe8
zotac <zotac@l505>
2012-11-15 13:08:06 +0300

    Added Calculating of elapsed time

--- Obj/CompTest.sh
+++ Obj/CompTest.sh
@@ -4,5 +4,5 @@ pgf95 -acc -Minfo -fast -g -c ../Src/testpoison.F -I/usr/local/cuda/include -o .
 pgf95 -acc -Minfo -fast -g -c ../Src/poison.F -o ./poison.o
 pgf95 -acc -Minfo -fast -g -c ../Src/poison_orig.F -o ./poison_orig.o
 
-pgf95  testpoison.o poison.o poison_orig.o precision.o parallel.o sys.o alloc.o fft.o chkgmx.o cellsubs.o parallelsubs.o mesh.o schecomm.o domain_decom.o spatial.o qsort.o io.o mmio.o sparse_matrices.o printmatrix.o pspltm1.o lenstr.o -L$PWD/fdf -L$PWD/SiestaXC -lSiestaXC -lfdf -o testpoison
+pgf95  testpoison.o poison.o poison_orig.o timer.o precision.o parallel.o sys.o alloc.o fft3d.o parallelsubs.o schecomm.o domain_decom.o spatial.o qsort.o io.o mmio.o sparse_matrices.o printmatrix.o pspltm1.o volcel.o reclat.o m_walltime.o lenstr.o pxf.o memory.o minvec.o sorting.o memoryinfo.o chkgmx.o -L$PWD/fdf -L$PWD/SiestaXC -lfdf -Mcuda -lblas -o testpoison
 
